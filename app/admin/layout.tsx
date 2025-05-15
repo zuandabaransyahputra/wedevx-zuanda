a74df926c086e95a308e9c77d5af6e8cb17c1cdc
zuandabaransyahputra <zuandabaransyahputra@gmail.com>
2025-05-16 05:17:29 +0700

    [UPDATE] - fix erro build

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,13 +1,15 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, Suspense } from "react";
 import Sidebar from "./_components/sidebar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col lg:flex-row">
-      <Sidebar />
-      <main className="flex-1 overflow-auto p-4">{children}</main>
-    </div>
+    <Suspense>
+      <div className="min-h-screen flex flex-col lg:flex-row">
+        <Sidebar />
+        <main className="flex-1 overflow-auto p-4">{children}</main>
+      </div>
+    </Suspense>
   );
 };
 
